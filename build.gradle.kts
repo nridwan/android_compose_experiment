a464a5a0592b8fb6cd18b70f39217206d1e7bc39
Nie Ridwan Susanto <xa18ridwan@gmail.com>
2022-08-31 18:28:02 +0700

    fix: update JDK ver and build system

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.19")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.43.2")
+    }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.google.protobuf") version "0.8.18" apply false
-    id("com.google.dagger.hilt.android") version "2.43.2" apply false
 }
 
 tasks.create<Delete>("clean") {
